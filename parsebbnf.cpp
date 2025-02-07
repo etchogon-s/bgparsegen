a142c0696021b25d4698d9936feb703e8d893fe7
etchogon-s <195778650+etchogon-s@users.noreply.github.com>
2025-02-07 14:01:17 +0000

    Added main function

--- parsebbnf.cpp
+++ parsebbnf.cpp
@@ -1,10 +1,8 @@
 #include <cctype>
-#include <cstdio>
 #include <deque>
 #include <iostream>
 #include <memory>
 #include <string>
-#include <string.h>
 #include <vector>
 
 enum TOKEN_TYPE {
@@ -309,4 +307,27 @@ static NodeList parseGrammar() {
         disjList.push_back(parseDisj());
     } while (!match(EOF_TOK));
     return disjList;
+}
+
+// Get input file and user's algorithm choice
+int main(int argc, char **argv) {
+    if (argc == 3) {
+        bbnfFile = fopen(argv[1], "r");
+        if (bbnfFile == NULL)
+            std::cout << "Error opening file\n";
+    } else {
+        std::cout << "Usage: ./code <input file> <algorithm>\n";
+        return 1;
+    }
+
+    // Initialise line and column numbers
+    lineNo = 1;
+    columnNo = 1;
+
+    // Run parser
+    auto grammarAST = parseGrammar();
+    std::cout << "Parsing finished\n";
+
+    fclose(bbnfFile);
+    return 0;
 }
